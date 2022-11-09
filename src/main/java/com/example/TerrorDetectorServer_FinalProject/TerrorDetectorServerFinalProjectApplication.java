eabfc18c23722ec8c21aa654ebea348caa3d4f60
Yarden Dahan <68491359+Jordan-me@users.noreply.github.com>
2022-11-09 14:12:26 +0200

    Initial commit

---
+++ src/main/java/com/example/TerrorDetectorServer_FinalProject/TerrorDetectorServerFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.TerrorDetectorServer_FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrorDetectorServerFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerrorDetectorServerFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TerrorDetectorServer_FinalProject/TerrorDetectorServerFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TerrorDetectorServer_FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrorDetectorServerFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
